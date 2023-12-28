6f1edf701381ef3e512ec3e5604eac185b1282bf
Monkey-Lufy <monkey.lufy.023@gmail.com>
2023-12-28 15:05:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TextForm from "./components/TextForm";
 import About from "./components/About";
 import React, { useState } from "react";
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   const [themeMode, setthemeMode] = useState("light");
