cd833fbbbd61a32079176515dd80cb1df26e410a
Monkey-Lufy <monkey.lufy.023@gmail.com>
2023-12-28 15:34:48 +0530

    removing react routing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import "./App.css";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import About from "./components/About";
+// import About from "./components/About";
 import React, { useState } from "react";
 
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [themeMode, setthemeMode] = useState("light");
@@ -36,41 +36,46 @@ function App() {
 
   return (
     <>
-      <Router>
-        {/* navbar component */}
-        <Navbar
-          title="TextUtils"
+      {/* <Router> */}
+      {/* navbar component */}
+      <Navbar
+        title="TextUtils"
+        themeMode={themeMode}
+        toggleThemeMode={toggleThemeMode}
+      />
+
+      {/* alert component */}
+      <Alert alert={alert} />
+
+      <div className="container my-4">
+        <TextForm
+          title="Enter The Text to Analyze"
           themeMode={themeMode}
-          toggleThemeMode={toggleThemeMode}
+          showAlert={showAlert}
         />
+        {/* <Routes> */}
+        {/* About component */}
+        {/* <Route */}
+        {/* exect */}
+        {/* path="/about" */}
+        {/* element={<About themeMode={themeMode} showAlert={showAlert} />} */}
+        {/* /> */}
 
-        {/* alert component */}
-        <Alert alert={alert} />
-
-        <div className="container my-4">
-          <Routes>
-            {/* About component */}
-            <Route
-              exect
-              path="/about"
-              element={<About themeMode={themeMode} showAlert={showAlert} />}
-            />
-
-            {/* Textform - Home page Component */}
-            <Route
-              exect
-              path="/"
-              element={
-                <TextForm
-                  title="Enter The Text to Analyze"
-                  themeMode={themeMode}
-                  showAlert={showAlert}
-                />
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
+        {/* Textform - Home page Component */}
+        {/* <Route */}
+        {/* exect */}
+        {/* path="/" */}
+        {/* element={ */}
+        {/* <TextForm */}
+        {/* title="Enter The Text to Analyze" */}
+        {/* themeMode={themeMode} */}
+        {/* showAlert={showAlert} */}
+        {/* /> */}
+        {/* } */}
+        {/* /> */}
+        {/* </Routes> */}
+      </div>
+      {/* </Router> */}
     </>
   );
 }
